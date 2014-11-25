89f133327a65cf8df4503d9c73e12a053cc97784
Carlos Fonseca <fonseca.carlosh@gmail.com>
2014-11-25 01:49:02 +0000

    Commit final

--- principal.js
+++ principal.js
@@ -1,13 +1,54 @@
-// Faça seu exercício neste arquivo
+// Faï¿½a seu exercï¿½cio neste arquivo
 
 
-// Alguns elementos importantes na página (index.html):
-// #rolar: Botão rolar
-//	 - você deve atribuir um _handler_ de evento a ele para fazer o cálculo da rolagem dos dados
-// #resultado: Elemento cujo conteúdo deve ser uma string com os resultados
-//	 - você deve definir seu conteúdo (.innerHTML) com a string de resultados
+// Alguns elementos importantes na pï¿½gina (index.html):
+// #rolar: Botï¿½o rolar
+//	 - vocï¿½ deve atribuir um _handler_ de evento a ele para fazer o cï¿½lculo da rolagem dos dados
+// #resultado: Elemento cujo conteï¿½do deve ser uma string com os resultados
+//	 - vocï¿½ deve definir seu conteï¿½do (.innerHTML) com a string de resultados
 // #recipienteResultados: "Container" do elemento dos resultados: 
-//	 - você deve torná-lo visível removendo a classe "oculto" dele
+//	 - vocï¿½ deve tornï¿½-lo visï¿½vel removendo a classe "oculto" dele
 // .quantidade: todos os input[type=number] com a quantidade de dados a serem rolados
 // #quantidadeD{4,6,8,10,12,20}: um ID para cada input[type=number] com a quantidade
+
+
+var rolar = document.getElementById('rolar');
+var quantidades = document.getElementsByClassName('dado');
+
+rolar.addEventListener('click', function(){
+    var soma = 0;
+    var frase = "";
+    for(var i = 0; i < quantidades.length; i++){
+        var max = 0;
+        if(i == 5){
+            max = 20
+        }else{
+            max = 4+(i*2);
+        }
+        var dado = document.getElementById('quantidadeD'+max.toString());
+        var qntde = dado.value;
+        for(var j = 0; j<qntde; j++){
+            if(frase != ""){
+                 frase += ' + '
+            }
+            var resultado = Math.ceil(Math.random() * max);
+            soma += resultado;
+            frase += resultado;  
+        }
+        
+    }
+    if(frase != ""){
+            
+        frase+=" = "+soma;
+        var container = document.getElementById('recipienteResultados');
+        container.classList.remove('oculto');
+        
+        var resultadoFrase = document.getElementById('resultado');
+        resultadoFrase.innerHTML = frase;
+    }
+    
+    
+});
+
+
 
